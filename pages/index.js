7967e28daa8e0ccd32f7b02c4bd4ccd1d3c08137
anand790335 <75306702+anand790335@users.noreply.github.com>
2024-03-25 17:53:30 +0530

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect, useContext } from "react";
+
+//INTERNAL IMPORTS
+import { ChatAppContext } from "../Context/ChatAppContext";
+
+const ChatApp = () => {
+  return <div>Hey</div>;
+};
+export default ChatApp;
